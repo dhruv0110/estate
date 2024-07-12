272cc5e512418be8f72c5846b97fc46470e2aa9a
DHRUV SHETH <dhruvsheth01102003@gmail.com>
2024-07-13 01:00:48 +0530

    my-new-practice

---
+++ src/components/GetStarted/GetStarted.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./GetStarted.css"
+
+const GetStarted = () => {
+  return (
+    <div>
+      <div className="g-wrapper">
+        <div className="paddings innerWidth g-container">
+            <div className="flexColCenter inner-container">
+                <span className='primaryText'>Get Started with Homyz</span>
+                <span className='secondaryText'>Subscribe and find super attractive price quotes from us.
+                    <br/>
+                    Find your residence soon
+                </span>
+                <button className='button'>
+                  <a href="mailto:dhruvsheth01102003@gmail.com">Get Started</a>
+                </button>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default GetStarted
